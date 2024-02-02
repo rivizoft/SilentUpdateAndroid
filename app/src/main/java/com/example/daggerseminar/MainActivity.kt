6fe62791c451cae4cabd1d462e854887eaf2f3aa
k.shiryaev <k.shiryaev@tinkoff.ru>
2024-02-02 17:12:04 +0500

    fix battery optimization

--- app/src/main/java/com/example/daggerseminar/MainActivity.kt
+++ app/src/main/java/com/example/daggerseminar/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.daggerseminar
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -30,22 +37,30 @@ class MainActivity : AppCompatActivity() {
 
         val isLastVersion = BuildConfig.VERSION_CODE >= 3
         findViewById<TextView>(R.id.lastVersion).apply {
-            this.visibility = if(isLastVersion) {
+            this.visibility = if (isLastVersion) {
                 View.VISIBLE
             } else {
                 View.GONE
             }
         }
 
+        findViewById<TextView>(R.id.modelNameText).apply {
+            text = "Модель телефона: ${Build.BRAND}"
+        }
+
         findViewById<Button>(R.id.installButton).apply {
-            this.visibility = if(isLastVersion) {
-                View.GONE
-            } else {
-                View.VISIBLE
-            }
             setOnClickListener {
-                it.visibility = View.GONE
                 appUpdater.update()
+            }
+        }
+
+        findViewById<TextView>(R.id.addToBackgroundButton).apply {
+            setOnClickListener {
+                if (allowInBackgroundMode().not()) {
+                    openSettingsBackgroundRestricted()
+                } else {
+                    openOptimizedStatusSettingsScreen()
+                }
             }
         }
 
@@ -74,13 +89,78 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        val isWorkingInBg = allowInBackgroundMode()
+
+        findViewById<TextView>(R.id.inBackgroundWorkAllowedText).apply {
+            visibility = if (isWorkingInBg) View.VISIBLE else View.GONE
+        }
+        findViewById<TextView>(R.id.inBackgroundWorkRestrictedText).apply {
+            visibility = if (isWorkingInBg.not()) View.VISIBLE else View.GONE
+        }
+        findViewById<TextView>(R.id.addToBackgroundButton).apply {
+            text = if (isWorkingInBg) {
+                "Убрать из фоновой работы"
+            } else {
+                "Добавить в фоновую работу"
+            }
+        }
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel() {
         val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        val channel = NotificationChannel(App.CHANNEL_ID, "App update channel", NotificationManager.IMPORTANCE_HIGH)
+        val channel = NotificationChannel(
+            App.CHANNEL_ID,
+            "App update channel",
+            NotificationManager.IMPORTANCE_HIGH
+        )
         notificationManager.createNotificationChannel(channel)
     }
 
+    private fun allowInBackgroundMode(): Boolean {
+        return runCatching {
+            val pm = getSystemService(Context.POWER_SERVICE) as PowerManager
+            pm.isIgnoringBatteryOptimizations(packageName)
+        }.getOrElse {
+            showErrorToast("Ошибка в получении статуса работы в фоне: $it")
+            false
+        }
+    }
+
+    @SuppressLint("BatteryLife")
+    private fun openSettingsBackgroundRestricted() {
+        runCatching {
+            val intent = Intent(
+                Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
+                Uri.fromParts("package", packageName, null)
+            )
+            startActivity(intent)
+        }.onFailure {
+            showErrorToast("Ошибка на добавление в фон: $it")
+        }
+    }
+
+    private fun openOptimizedStatusSettingsScreen() {
+        runCatching {
+            val intent = Intent(
+                Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS,
+            )
+            startActivity(intent)
+        }.onFailure {
+            showErrorToast("Ошибка на редирект отключения из фона: $it")
+        }
+    }
+
+    private fun showErrorToast(errorText: String) {
+        Toast.makeText(
+            this,
+            errorText,
+            Toast.LENGTH_LONG
+        ).show()
+    }
 
 }
 
